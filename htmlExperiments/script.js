0d9640c3e1d2b23005c28d1fe61b4dd627a17193
Avynn <avynn@donaghe.com>
2018-01-11 17:03:19 -0800

    working web client moving on to serving web client

--- htmlExperiments/script.js
+++ htmlExperiments/script.js
@@ -1,4 +1,5 @@
 var ws = new WebSocket("ws://localhost:8080/chat");
+let username = window.prompt("Choose a username");
 
 ws.onerror = function (event) {
     console.log(event.data);
@@ -17,9 +18,30 @@ ws.onopen = function(event) {
     console.log("connection established!");
 
     let objToSend = {
-        "username": "html client",
+        "username": username,
         "message": "hello world!"
     }
 
     ws.send(JSON.stringify(objToSend));
 }
+
+$(function () {
+    $("#sendMessageBox").keypress(function (event) {
+        var message = ""
+    
+        if(event["key"] == "Enter"){
+            message = $(this).val();
+
+            console.log(message);
+    
+            let objToSend = {
+                "username": username,
+                "message": message
+            }
+    
+            ws.send(JSON.stringify(objToSend));
+    
+            $(this).val("");
+        }
+    });
+});
